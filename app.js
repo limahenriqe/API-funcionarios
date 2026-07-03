471144827dc3ae506d4ba4c4b5f7d79a410b16d5
Henrique Lima <henriqueslima.08@gmail.com>
2026-07-03 17:58:59 -0300

    Testando pipeline

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express")
+
+const app = express()
+
+app.use(express.json())
+
+//banco de dados em memoria
+const funcionarios = []
+
+//rota para cadastrar
+app.post("/funcionarios", (req,res)=>{
+
+    const {nome, cargo} = req.body
+
+    const funcionario = {
+        nome, 
+        cargo
+    }
+
+    funcionarios.push(funcionario)
+
+    res.status(201).json(funcionario)
+})
+
+//rota para mostrar os funcionarios
+app.get("/funcionarios",(req,res)=>{
+
+    res.status(200).json(funcionarios)
+})
+
+module.exports = app
