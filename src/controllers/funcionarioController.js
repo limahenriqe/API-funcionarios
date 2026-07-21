48672c1790c5a930c653adc2cf3cba1b186a1656
Henrique Lima <henriqueslima.08@gmail.com>
2026-07-20 21:07:33 -0300

    Corrigindo teste e código

--- src/controllers/funcionarioController.js
+++ src/controllers/funcionarioController.js
@@ -1,13 +1,31 @@
+const { default: axios } = require("axios")
 const Funcionario = require("../models/funcionario")
 
-exports.cadastrar = async(req,res) => {
-    // insert into funcionario values (?,?,?)
-    const funcionario = await Funcionario.create(req.body)
-    res.status(201).json(funcionario) // 201 create
-}
+    exports.cadastrar = async(req,res) => {
+        
+        try {
 
-exports.listar = async(req,res) => {
-    const funcionarios = await Funcionario.findAll()
-    res.status(200).json(funcionarios) // 200 deu certo (get)
-}
+            const funcionario = await Funcionario.create(req.body)
 
+            await axios.post(`${process.env.API_BENEFICIOS}/beneficios`,{
+                funcionarioId: funcionario.id,
+                salario:funcionario.salario,
+                cargo: funcionario.cargo
+            })
+
+
+            res.status(201).json(funcionario)
+            
+        } catch (error) {
+            console.log(error)
+            res.status(500).json({mensage:"Erro de Servidor"})
+        }
+    
+    }
+
+    exports.listar = async(req,res) => {
+
+        //select * from funcionarios
+        const funcionarios = await Funcionario.findAll()
+        res.status(200).json(funcionarios)
+    }
